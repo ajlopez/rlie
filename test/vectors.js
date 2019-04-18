faa53e4383b3e78ddc5904212068e4308bdeaca0
ajlopez <webmaster@ajlopez.com>
2019-04-18 18:31:58 -0300

    Create vector with vectors

--- test/vectors.js
+++ test/vectors.js
@@ -6,6 +6,17 @@ exports['create vector'] = function (test) {
     
     test.ok(vector);
     test.equal(typeof vector, 'object');
+    test.equal(vector.length(), 3);
     test.deepEqual(vector.elements(), [ 1, 2, 3 ]);
+};
+
+exports['create vector with vectors'] = function (test) {
+    const v0 = vectors.vector([1, 2, 3]);
+    const vector = vectors.vector([ v0, 2, v0 ]);
+    
+    test.ok(vector);
+    test.equal(typeof vector, 'object');
+    test.equal(vector.length(), 7);
+    test.deepEqual(vector.elements(), [ 1, 2, 3, 2, 1, 2, 3 ]);
 };
 
