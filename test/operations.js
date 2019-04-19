643773c2f39fca4cfcee948e37c12ea93a5365da
ajlopez <webmaster@ajlopez.com>
2019-04-19 09:54:03 -0300

    Add number to vector

--- test/operations.js
+++ test/operations.js
@@ -1,13 +1,28 @@
 
 const operations = require('../lib/operations');
+const vectors = require('../lib/vectors');
 
 function add(test, x, y, expected) {
-    test.equal(operations.add(x, y), expected);
+    if (Array.isArray(y))
+        y = vectors.vector(y);
+    
+    const result = operations.add(x, y);
+    
+    if (vectors.isVector(result))
+        test.deepEqual(result.elements(), expected);
+    else
+        test.equal(result, expected);
 }
 
 exports['add numbers'] = function (test) {
     add(test, 1, 2, 3);
     add(test, Math.PI, Math.E, Math.PI + Math.E);
     add(test, -1, 1, 0);
+};
+
+exports['add number and vector'] = function (test) {
+    add(test, 1, [ 1, 2, 3 ], [ 2, 3, 4 ]);
+    add(test, Math.PI, [ Math.E, Math.E ], [ Math.PI + Math.E, Math.PI + Math.E ]);
+    add(test, -1, [ 1, 1, 1 ], [ 0, 0, 0 ]);
 };
 
