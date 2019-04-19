f0f647f3231c7bf4f108ed3da2cb9ee22ba1f892
ajlopez <webmaster@ajlopez.com>
2019-04-19 10:33:43 -0300

    Subtract operation

--- test/operations.js
+++ test/operations.js
@@ -2,19 +2,27 @@
 const operations = require('../lib/operations');
 const vectors = require('../lib/vectors');
 
-function add(test, x, y, expected) {
+function apply(test, opname, x, y, expected) {
     if (Array.isArray(x))
         x = vectors.vector(x);
     
     if (Array.isArray(y))
         y = vectors.vector(y);
     
-    const result = operations.add(x, y);
+    const result = operations[opname](x, y);
     
     if (vectors.isVector(result))
         test.deepEqual(result.elements(), expected);
     else
         test.equal(result, expected);
+}
+
+function add(test, x, y, expected) {
+    apply(test, 'add', x, y, expected);
+}
+
+function subtract(test, x, y, expected) {
+    apply(test, 'subtract', x, y, expected);
 }
 
 exports['add numbers'] = function (test) {
@@ -41,3 +49,29 @@ exports['add vector and vector'] = function (test) {
     add(test, [ 1, 1, 1 ], [ -1, 1 ], [ 0, 2, 0 ]);
     add(test, [ -1, 1 ], [ 1, 1, 1 ], [ 0, 2, 0 ]);
 };
+
+exports['subtract numbers'] = function (test) {
+    subtract(test, 1, 2, -1);
+    subtract(test, Math.PI, Math.E, Math.PI - Math.E);
+    subtract(test, -1, 1, -2);
+};
+
+exports['subtract number and vector'] = function (test) {
+    subtract(test, 1, [ 1, 2, 3 ], [ 0, -1, -2 ]);
+    subtract(test, Math.PI, [ Math.E, Math.E ], [ Math.PI - Math.E, Math.PI - Math.E ]);
+    subtract(test, -1, [ 1, 1, 1 ], [ -2, -2, -2 ]);
+};
+
+exports['subtract vector and number'] = function (test) {
+    subtract(test, [ 1, 2, 3 ], 1, [ 0, 1, 2 ]);
+    subtract(test, [ Math.E, Math.E ], Math.PI, [ Math.E - Math.PI, Math.E - Math.PI ]);
+    subtract(test, [ 1, 1, 1 ], -1, [ 2, 2, 2 ]);
+};
+
+exports['subtract vector and vector'] = function (test) {
+    subtract(test, [ 1, 2, 3 ], [ 1, 2, 3 ], [ 0, 0, 0 ]);
+    subtract(test, [ Math.E, Math.E ], [ 1, Math.PI ], [ Math.E - 1, Math.E - Math.PI ]);
+    subtract(test, [ 1, 1, 1 ], [ -1, 1 ], [ 2, 0, 2 ]);
+    subtract(test, [ -1, 1 ], [ 1, 1, 1 ], [ -2, 0, -2 ]);
+};
+
