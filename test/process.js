e0295c00008599aabdad2d02a7847ea4f2901265
ajlopez <webmaster@ajlopez.com>
2019-04-20 10:00:51 -0300

    Process name

--- test/process.js
+++ test/process.js
@@ -1,11 +1,12 @@
 
 const interpreter = require('../lib/interpreter');
 const parser = require('../lib/parser');
+const contexts = require('../lib/contexts');
 
-function process(test, text, expected) {
+function process(test, text, expected, context) {
     const node = parser.parse('expression', text);
     
-    test.deepEqual(interpreter.process(node), expected);
+    test.deepEqual(interpreter.process(node, context), expected);
 }
 
 exports['process constants'] = function (test) {
@@ -15,5 +16,12 @@ exports['process constants'] = function (test) {
     process(test, 'TRUE', true);
     process(test, 'FALSE', false);
     process(test, 'NULL', null);
+};
+
+exports['process name'] = function (test) {
+    const context = contexts.context();
+    context.set('answer', 42);
+    
+    process(test, 'answer', 42, context);
 };
 
