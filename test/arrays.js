4559437b58176e00c2fd08731e30b2279576a3ec
ajlopez <webmaster@ajlopez.com>
2019-04-24 13:46:45 -0300

    Resize array in creation if needed

--- test/arrays.js
+++ test/arrays.js
@@ -11,3 +11,13 @@ exports['create array with dimensions'] = function (test) {
     test.deepEqual(array.dimensions().elements(), [ 2, 2 ]);
 };
 
+exports['create array with dimensions extending data'] = function (test) {
+    const array = vectors.array([1, 2, 3, 4], [ 2, 3 ]);
+    
+    test.ok(array);
+    test.equal(typeof array, 'object');
+    test.equal(array.length(), 6);
+    test.deepEqual(array.elements(), [ 1, 2, 3, 4, 1, 2 ]);
+    test.deepEqual(array.dimensions().elements(), [ 2, 3 ]);
+};
+
