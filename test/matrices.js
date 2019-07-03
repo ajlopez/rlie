b52d1df7a007c3d6b0e84c42c2c4bc264c6d9f6a
ajlopez <webmaster@ajlopez.com>
2019-07-03 12:53:29 -0300

    Matrix first test, WIP

---
+++ test/matrices.js
@@ -0,0 +1,20 @@
+
+const vectors = require('../lib/vectors');
+
+exports['create matrix from native array'] = function (test) {
+    const matrix = vectors.matrix([ 1, 2, 3, 4, 5, 6 ], 2, 3);
+    
+    test.ok(matrix);
+    test.equal(matrix.length(), 6);
+    test.deepEqual(matrix.elements(), [ 1, 2, 3, 4, 5, 6 ]);
+    // TODO resolve this test
+    //test.deepEqual(matrix.dimensions(), [ 2, 3 ]);
+
+    test.equal(matrix.get(1, 1), 1);
+    test.equal(matrix.get(2, 1), 2);
+    test.equal(matrix.get(1, 2), 3);
+    test.equal(matrix.get(2, 2), 4);
+    test.equal(matrix.get(1, 3), 5);
+    test.equal(matrix.get(2, 3), 6);
+};
+
