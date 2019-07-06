5d86e3d745928705d996f2efcdd63e0420833fe4
ajlopez <webmaster@ajlopez.com>
2019-07-06 12:50:00 -0300

    Create matrix without specifying no of rows, columns

--- test/matrices.js
+++ test/matrices.js
@@ -17,3 +17,19 @@ exports['create matrix from native array'] = function (test) {
     test.equal(matrix.get(2, 3), 6);
 };
 
+exports['create matrix from vector without specifying rows and columns'] = function (test) {
+    const matrix = vectors.matrix(vectors.vector([ 1, 2, 3, 4, 5, 6 ]));
+    
+    test.ok(matrix);
+    test.equal(matrix.length(), 6);
+    test.deepEqual(matrix.elements(), [ 1, 2, 3, 4, 5, 6 ]);
+    test.deepEqual(matrix.dimensions().elements(), [ 6, 1 ]);
+
+    test.equal(matrix.get(1, 1), 1);
+    test.equal(matrix.get(2, 1), 2);
+    test.equal(matrix.get(3, 1), 3);
+    test.equal(matrix.get(4, 1), 4);
+    test.equal(matrix.get(5, 1), 5);
+    test.equal(matrix.get(6, 1), 6);
+};
+
