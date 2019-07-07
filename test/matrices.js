84de5fcd5023f69eaa5d9b45ba6bab6169668ee1
ajlopez <webmaster@ajlopez.com>
2019-07-07 10:28:09 -0300

    Rbind matrix creation

--- test/matrices.js
+++ test/matrices.js
@@ -33,3 +33,51 @@ exports['create matrix from vector without specifying rows and columns'] = funct
     test.equal(matrix.get(6, 1), 6);
 };
 
+exports['create matrix using native array rows'] = function (test) {
+    const matrix = vectors.rbind([ 1, 2, 3 ], [ 4, 5, 6 ]);
+    
+    test.ok(matrix);
+    test.equal(matrix.length(), 6);
+    test.deepEqual(matrix.elements(), [ 1, 2, 3, 4, 5, 6 ]);
+    test.deepEqual(matrix.dimensions().elements(), [ 2, 3 ]);
+
+    test.equal(matrix.get(1, 1), 1);
+    test.equal(matrix.get(2, 1), 2);
+    test.equal(matrix.get(1, 2), 3);
+    test.equal(matrix.get(2, 2), 4);
+    test.equal(matrix.get(1, 3), 5);
+    test.equal(matrix.get(2, 3), 6);
+};
+
+exports['create matrix using native array rows with different lenghts'] = function (test) {
+    const matrix = vectors.rbind([ 1, 2 ], [ 4, 5, 6 ]);
+    
+    test.ok(matrix);
+    test.equal(matrix.length(), 6);
+    test.deepEqual(matrix.elements(), [ 1, 2, 1, 4, 5, 6 ]);
+    test.deepEqual(matrix.dimensions().elements(), [ 2, 3 ]);
+
+    test.equal(matrix.get(1, 1), 1);
+    test.equal(matrix.get(2, 1), 2);
+    test.equal(matrix.get(1, 2), 1);
+    test.equal(matrix.get(2, 2), 4);
+    test.equal(matrix.get(1, 3), 5);
+    test.equal(matrix.get(2, 3), 6);
+};
+
+exports['create matrix using vector rows'] = function (test) {
+    const matrix = vectors.rbind(vectors.vector([ 1, 2, 3 ]), vectors.vector([ 4, 5, 6 ]));
+    
+    test.ok(matrix);
+    test.equal(matrix.length(), 6);
+    test.deepEqual(matrix.elements(), [ 1, 2, 3, 4, 5, 6 ]);
+    test.deepEqual(matrix.dimensions().elements(), [ 2, 3 ]);
+
+    test.equal(matrix.get(1, 1), 1);
+    test.equal(matrix.get(2, 1), 2);
+    test.equal(matrix.get(1, 2), 3);
+    test.equal(matrix.get(2, 2), 4);
+    test.equal(matrix.get(1, 3), 5);
+    test.equal(matrix.get(2, 3), 6);
+};
+
