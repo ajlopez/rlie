296d6666c93d2dbd4e37930826fcb8e50d3da4e7
ajlopez <webmaster@ajlopez.com>
2019-07-29 11:38:47 -0300

    Integer division as operator

--- test/lexers.js
+++ test/lexers.js
@@ -347,3 +347,15 @@ exports['modulus as operator'] = function (test) {
     test.equal(lexer.next(), null);
 };
 
+exports['integer division as operator'] = function (test) {
+    const lexer = lexers.lexer('%/%');
+  
+    const token = lexer.next();
+    
+    test.ok(token);
+    test.equal(token.value, '%/%');
+    test.equal(token.type, TokenType.Operator);
+    
+    test.equal(lexer.next(), null);
+};
+
